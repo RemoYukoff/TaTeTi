f3cd1ce04a9dea26ed54bba654d637b230b419a2
Remo <basilisco.125@hotmail.com.ar>
2018-09-17 00:45:41 -0300

    Código comentado, mejorado e imágenes agregadas

--- tateti.py
+++ tateti.py
@@ -1,89 +1,108 @@
 from tkinter import *
-import time
-#Clase tablero crea la app y realiza el tablero
-class Tablero():
-    #Matriz para las celdas
-    matriz=[]
-    def __init__(self):
-        self.app=Tk()
-        self.app.geometry("612x612")
-        self.app.resizable(False,False)
-        self.app.title("TaTeTi")
-        for a in range(3):
-            for b in range(3):
-                new=Celda(self.app,a,b)
-                Tablero.matriz.append(new)
+from tkinter import messagebox
 
-#Clase celda maneja cada celda
-class Celda():
-    #Variable para saber quien juega turn=0 Juega cruz / turn=1 Juega circulo
-    turn=0
-    def __init__(self,app,row,column):
-        self.canvas=Canvas(app,width=200,height=200,bg="black")
-        self.canvas.bind("<Button-1>",self.xo)
-        self.canvas.grid(row=row,column=column)
-        self.drawed=0
-        self.value="1"
-        self.app=app
+#Funcion para cambiar la imagen de un label
+def changeimage(label,path):
+    image=PhotoImage(file=path)
+    label.configure(image=image)
+    label.image=image
 
+#Ventana principal
+class Container(Tk):
+    def __init__(self,**kwargs):
+        super(Container,self).__init__(**kwargs)
+        img=Image("photo",file="X.png") #Imagen para icono
+        self.call('wm','iconphoto',self._w,img) #Icono de la ventana
+        self.geometry("540x540+%i+%i"%(self.winfo_screenwidth()/2-277,self.winfo_screenheight()/2-277)) #Tamaño y posición inicial
+        self.update_idletasks() #Actualiza la posición de la ventana
+        self.title("TaTeTi") #Tìtulo
+        self.configure(bg="#0d292f") #Fondo de la lìneas
+        self.minsize(540,540) #Tamaño mìnimo
+        #Configurar las filas y columnas para que se ajusten al tamaño de la ventana
+        for row in range(3):
+            self.rowconfigure(row,weight=1)
+        for column in range(3):
+            self.columnconfigure(column,weight=1)
+
+        self.tablero()
+    #Crea un tablero e iniciar turno
+    def tablero(self):
+        self.matriz=[] #Matriz para las celdas
+        self.turn=0 #Variable para saber quien juega / 0 Juega cruz / 1 Juega circulo /
+        #Crear tablero
+        for row in range(3):
+            for column in range(3):
+                new=Celda()
+                new.grid(row=row,column=column,sticky="wens",pady=1,padx=1)
+                self.matriz.append(new)
+
+class Celda(Label):
+    def __init__(self,**kwargs):
+        super(Celda,self).__init__(**kwargs)
+        self.configure(bg="black") #Fondo negro
+        changeimage(self,"None.png") #Imagen inicial en negro
+        self.bind("<Button-1>",self.xo) #Enlaza el click izquierdo al evento de control
+        self.drawed=0 #Controla si la casilla esta dibujada
+        self.value=0 #Valor X u O
+
+    #Evento de control
     def xo(self,event):
-        if Celda.turn==0 and self.drawed==0:
-            self.canvas.create_polygon(20,40,40,20,180,160,160,180,fill="#9f0e1f")
-            self.canvas.create_polygon(180,40,40,180,20,160,160,20,fill="#9f0e1f")
-            self.canvas.update_idletasks()
-            self.drawed=1
-            self.value="X"
-            Celda.turn=1
+        if self.master.turn == 0 and self.drawed == 0: #En turno de las X y se aprieta en casilla sin dibujar
+            changeimage(self,"X.png") #Cambiar imagen
+            self.drawed=1 #La casilla pasa a estar dibujada
+            self.value="X" #Cambia su valor
+            self.master.turn=1 #Cambia a el turno de las O
+            self.win() #Comprobar victoria
+        if self.master.turn == 1 and self.drawed == 0: #En turno de las O y se aprieta en casilla sin dibujar
+            changeimage(self,"O.png") #Cambiar imagen
+            self.drawed=1 #La casilla pasa a estar dibujada
+            self.value="O" #Cambia su valor
+            self.master.turn=0 #Cambia a el turno de las X
+            self.win() #Comprobar victoria
 
-        if Celda.turn==1 and self.drawed==0:
-            self.canvas.create_oval(30,30,170,170,outline="#22587a",width=20)
-            self.canvas.update_idletasks()
-            self.drawed=1
-            self.value="O"
-            Celda.turn=0
-        self.win()
-
+    #Controla si gana alguien
     def win(self):
-        m=Tablero.matriz
+        m=self.master.matriz
         #Fila sup
-        if m[0].value!="1" and m[0].value==m[1].value==m[2].value:
-            time.sleep(0.1)
-            Frame(self.app,bg="").grid(row=0,column=0,rowspan=3,columnspan=3,sticky=W+E+N+S)
-            Label(self.app,text="GANAN LAS "+m[0].value,font=["",30],bg="black",fg="#908f28").grid(row=1,column=0,columnspan=3,sticky=W+E+N+S)
+        if m[0].value!=0 and m[0].value == m[1].value == m[2].value:
+            self.end(m[0].value)
         #Fila med
-        if m[3].value!="1" and m[3].value==m[4].value==m[5].value:
-            time.sleep(0.1)
-            Frame(self.app,bg="").grid(row=0,column=0,rowspan=3,columnspan=3,sticky=W+E+N+S)
-            Label(self.app,text="GANAN LAS "+m[3].value,font=["",30],bg="black",fg="#908f28").grid(row=1,column=0,columnspan=3,sticky=W+E+N+S)
+        if m[3].value!=0 and m[3].value == m[4].value == m[5].value:
+            self.end(m[3].value)
         #Fila inf
-        if m[6].value!="1" and m[6].value==m[7].value==m[8].value:
-            time.sleep(0.1)
-            Frame(self.app,bg="").grid(row=0,column=0,rowspan=3,columnspan=3,sticky=W+E+N+S)
-            Label(self.app,text="GANAN LAS "+m[6].value,font=["",30],bg="black",fg="#908f28").grid(row=1,column=0,columnspan=3,sticky=W+E+N+S)
+        if m[6].value!=0 and m[6].value == m[7].value == m[8].value:
+            self.end(m[6].value)
         #Columna izq
-        if m[0].value!="1" and m[0].value==m[3].value==m[6].value:
-            time.sleep(0.1)
-            Frame(self.app,bg="").grid(row=0,column=0,rowspan=3,columnspan=3,sticky=W+E+N+S)
-            Label(self.app,text="GANAN LAS "+m[0].value,font=["",30],bg="black",fg="#908f28").grid(row=1,column=0,columnspan=3,sticky=W+E+N+S)
+        if m[0].value!=0 and m[0].value == m[3].value == m[6].value:
+            self.end(m[0].value)
         #Columna med
-        if m[1].value!="1" and m[1].value==m[4].value==m[7].value:
-            time.sleep(0.1)
-            Frame(self.app,bg="").grid(row=0,column=0,rowspan=3,columnspan=3,sticky=W+E+N+S)
-            Label(self.app,text="GANAN LAS "+m[1].value,font=["",30],bg="black",fg="#908f28").grid(row=1,column=0,columnspan=3,sticky=W+E+N+S)
+        if m[1].value!=0 and m[1].value == m[4].value == m[7].value:
+            self.end(m[1].value)
         #Columna der
-        if m[2].value!="1" and m[2].value==m[5].value==m[8].value:
-            time.sleep(0.1)
-            Frame(self.app,bg="").grid(row=0,column=0,rowspan=3,columnspan=3,sticky=W+E+N+S)
-            Label(self.app,text="GANAN LAS "+m[2].value,font=["",30],bg="black",fg="#908f28").grid(row=1,column=0,columnspan=3,sticky=W+E+N+S)
+        if m[2].value!=0 and m[2].value == m[5].value == m[8].value:
+            self.end(m[2].value)
         #Diagonal principal
-        if m[0].value!="1" and m[0].value==m[4].value==m[8].value:
-            time.sleep(0.1)
-            Frame(self.app,bg="").grid(row=0,column=0,rowspan=3,columnspan=3,sticky=W+E+N+S)
-            Label(self.app,text="GANAN LAS "+m[2].value,font=["",30],bg="black",fg="#908f28").grid(row=1,column=0,columnspan=3,sticky=W+E+N+S)
+        if m[0].value!=0 and m[0].value == m[4].value == m[8].value:
+            self.end(m[0].value)
         #Diagonal secundaria
-        if m[2].value!="1" and m[2].value==m[4].value==m[6].value:
-            time.sleep(0.1)
-            Frame(self.app,bg="").grid(row=0,column=0,rowspan=3,columnspan=3,sticky=W+E+N+S)
-            Label(self.app,text="GANAN LAS "+m[2].value,font=["",50],bg="black",fg="#908f28").grid(row=1,column=0,columnspan=3,sticky=W+E+N+S)
-Tablero()
+        if m[2].value!=0 and m[2].value == m[4].value == m[6].value:
+            self.end(m[2].value)
+        #Empate
+        if m[0].drawed==m[1].drawed==m[2].drawed==m[3].drawed==m[4].drawed==m[5].drawed==m[6].drawed==m[7].drawed==m[8].drawed==1:
+            self.end("draw")
+
+    #Genera los mensajes cuando se termina la partida
+    def end(self,winner):
+        if winner=="draw":
+            message=messagebox.askokcancel("EMPATE","¡EMPATE!\n¿Volver a jugar?")
+        else:
+            message=messagebox.askokcancel("GANAN LAS "+winner,"¡GANAN LAS "+winner+"!\n¿Volver a jugar?")
+        if message: #Destruir todo y crear nuevo tablero
+            for celda in self.master.matriz:
+                celda.destroy()
+            self.master.tablero()
+        else: #Salir
+            self.master.quit()
+
+Container()
 mainloop()
